a4427e12a0edb518457d9560d7a7f700620d95f8
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-07 08:56:16 -0300

    feat: created and configured token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './views/pages.module';
 import { ExibicaoDeListaDeUsuariosComponent } from './views/exibicao-de-lista-de-usuarios/exibicao-de-lista-de-usuarios.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './core/interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,12 @@ import { ExibicaoDeListaDeUsuariosComponent } from './views/exibicao-de-lista-de
     AppRoutingModule,
     PagesModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = '6630e25ac7ec1fee3a5d2d61';
+    const authRequest = request.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+
+    return next.handle(authRequest);
+  }
+}
