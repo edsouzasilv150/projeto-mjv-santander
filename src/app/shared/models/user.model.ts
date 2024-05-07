2248682508bb2d3cbb9c6c23bde74ce2ad155cf0
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-07 11:22:08 -0300

    created model

---
+++ src/app/shared/models/user.model.ts
@@ -0,0 +1,21 @@
+export interface IUser {
+  id?: number,
+  title?: string,
+  firstName: string,
+  lastName: string,
+  gender?: string,
+  email: string,
+  dateOfBirth?: string,
+  registerDate?: string,
+  phone?: string,
+  picture: string,
+  location?: ILocation[]
+}
+
+export interface ILocation {
+  street: string,
+  city: string,
+  state: string,
+  country: string,
+  timezone: string
+}
