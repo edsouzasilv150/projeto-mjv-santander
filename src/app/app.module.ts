966bf5d5d8b313c22d738a076496a7ddc4cf0394
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-06 22:22:09 -0300

    refacture: structuring folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesModule } from './views/pages.module';
+import { ExibicaoDeListaDeUsuariosComponent } from './views/exibicao-de-lista-de-usuarios/exibicao-de-lista-de-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExibicaoDeListaDeUsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/pages.module.ts
+++ src/app/views/pages.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ExibicaoDeListaDeUsuariosComponent } from './exibicao-de-lista-de-usuarios/exibicao-de-lista-de-usuarios.component';
-
-
 
 @NgModule({
   declarations: [
-    ExibicaoDeListaDeUsuariosComponent
+
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+
   ]
 })
 export class PagesModule { }
