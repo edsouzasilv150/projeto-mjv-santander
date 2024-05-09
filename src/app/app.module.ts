3d87604b1cead1d72771a12faad84a3893cb96e4
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-09 19:21:39 -0300

    add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { MatCardModule } from '@angular/material/card';
     PagesModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatCardModule,
+    MatCardModule
   ],
   providers: [
     {
