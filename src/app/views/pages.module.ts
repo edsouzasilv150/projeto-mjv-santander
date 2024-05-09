cdd3c5c1550b0392dcf2bea14e52066c19426e3e
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-09 19:25:23 -0300

    feat: add news modules

--- src/app/views/pages.module.ts
+++ src/app/views/pages.module.ts
@@ -3,18 +3,38 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { UsuarioUnicoComponent } from './usuario-unico/usuario-unico.component';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CriarUsuarioComponent } from './criar-usuario/criar-usuario.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { NgxFileDropModule } from 'ngx-file-drop';
+import {MatIconModule} from '@angular/material/icon';
 
 
+export const options: Partial<null|IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
-  declarations: [UsuarioUnicoComponent],
+  declarations: [
+    UsuarioUnicoComponent,
+    CriarUsuarioComponent,
+  ],
   imports: [
+    NgxMaskModule.forRoot(),
     CommonModule,
     PagesRoutingModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgxFileDropModule,
+    MatIconModule
   ],
   exports: [
   ],
